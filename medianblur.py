dba2aba6cfecb1e33dcb944234f846eede1f9c1f
0919Zeng <13261301988@163.com>
2019-10-17 23:49:15 +0100

    median blur

--- medianblur.py
+++ medianblur.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def medianBlur(img, kernel):
+def medianBlur(img, kernel, padding_way='ZERO'):
     # kernel的值为奇数
     if kernel % 2 == 0 or kernel is 1:
         print('kernel must be odd')
@@ -15,11 +15,24 @@ def medianBlur(img, kernel):
     if channel == 3:  # 三通道拆成单通道
         matrix_3 = np.zeros_like(img)
         for l in range(matrix_3.shape[2]):
-            matrix_3[:, :, l] = medianBlur(img[:, :, l], kernel)
+            matrix_3[:, :, l] = medianBlur(img[:, :, l], kernel, padding_way)
         return matrix_3
     elif channel == 2:  # 单通道
         matrix = np.zeros((height + padding * 2, width + padding * 2), dtype=img.dtype)
         matrix[padding:-padding, padding:-padding] = img
+
+        #padding way
+        if padding_way is 'ZERO':
+            pass
+        elif padding_way is 'REPLICA':
+            for r in range(padding):
+                matrix[r, padding:-padding] = img[0, :]
+                matrix[-(1 + r), padding:-padding] = img[-1, :]
+                matrix[padding:-padding, r] = img[:, 0]
+                matrix[padding:-padding, -(1 + r)] = img[:, -1]
+        else:
+            print('padding_way error need ZERO or REPLICA')
+            return None
 
         # 创建输出矩阵
         mat_median = np.zeros((height, width), dtype=img.dtype)
@@ -39,7 +52,7 @@ def medianBlur(img, kernel):
 
 def main():
     img = cv2.imread("lenna.png", 1)
-    img_median = medianBlur(img, 5)  #输入图片和卷积核尺寸
+    img_median = medianBlur(img, 5, padding_way='REPLICA')  #输入图片和卷积核尺寸
     cv2.imshow('lenna', img)
     cv2.imshow('medianBlur_lenna', img_median)
     cv2.waitKey(0)
